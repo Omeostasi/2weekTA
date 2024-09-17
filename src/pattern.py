3188d1292106cfb83c74d9b81da873e046c0d17f
Giovanni Vincenzo Cavallo <202402143@post.au.dk>
2024-09-17 14:50:58 +0200

    done it

--- src/pattern.py
+++ src/pattern.py
@@ -1,2 +1,12 @@
-
+for i in range(5):
+    for j in range(0, i): #i 0 j 0 doesnt i 1 j0 does print
+        if j!=i:
+            print('*', end=' ')
+    print('*')    
+for i in range(4, 0,-1):
+    for j in range(i, 0,-1): #i 0 j 0 doesnt i 1 j0 does print
+        if j!=i:
+            print('*', end=' ')
+    print('*')    
+        
 # Print the pattern
